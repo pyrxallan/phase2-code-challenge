fc534dcd5dedda5ef964d02ee7af1de11a82cd83
Allan Ratemo <allanisratemo@gmail.com>
2025-10-31 20:33:50 +0300

    Update team

--- src/components/MyTeam.jsx
+++ src/components/MyTeam.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PokemonCard from './PokemonCard';
 
-function MyTeam({ team, onRemoveFromTeam }) {
+function MyTeam({ team, onSelectPokemon, onRemoveFromTeam }) {
   return (
-    <div className="my-team">
-      {/* Selected team members will be displayed here */}
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3 mb-6">
+      {team.map(pkmn => (
+        <PokemonCard
+          key={pkmn.id}
+          pokemon={pkmn}
+          onClick={() => onSelectPokemon(pkmn)}
+          onRemove={onRemoveFromTeam}
+          showRemove={true}
+        />
+      ))}
+      {[...Array(6 - team.length)].map((_, i) => (
+        <div 
+          key={i} 
+          className="bg-white rounded-lg h-56 flex items-center justify-center border-2 border-dashed border-gray-300"
+        >
+          <span className="text-gray-400 font-medium text-sm">Empty</span>
+        </div>
+      ))}
     </div>
   );
 }
